fe19e932887a5469753a3cb088c63b6896f54dd0
Joe <Jhuntenburg@gmail.com>
2023-07-22 22:37:56 -0400

    arrayList tests

--- gettestie/src/test/java/TestArrayLists.java
+++ gettestie/src/test/java/TestArrayLists.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class TestArrayLists {
 
     @Test
-    public void testAdd(){
+    public void testAdd() {
         List<String> expected = new ArrayList<>();
         expected.add("Chicken");
         expected.add("Beef");
@@ -16,10 +16,39 @@ public class TestArrayLists {
         Assert.assertEquals(3, expected.size());
     }
 
+    @Test
+    public void testRemove() {
+        List<String> expected = new ArrayList<>();
+        expected.add("Chicken");
+        expected.add("Beef");
+        expected.add("Steak");
+//        when
+        expected.remove(0);
+//        then
+        Assert.assertEquals(2, expected.size());
+    }
 
+    @Test
+    public void testClear() {
+        List<String> expected = new ArrayList<>();
+        expected.add("Chicken");
+        expected.add("Beef");
+        expected.add("Steak");
 
+//    when
+        expected.clear();
+//        then
+        Assert.assertEquals(0, expected.size());
+    }
+    @Test
+    public void testContains(){
+        List<String> expected = new ArrayList<>();
+        expected.add("Chicken");
+        expected.add("Beef");
+        expected.add("Steak");
 
+        Assert.assertTrue(expected.contains("Steak"));
 
-
+    }
 
 }
